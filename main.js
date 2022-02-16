b7a94ab9ad80a4187119efe474c864f1ffca8e23
thomasalves11 <thomas.alves@agisrn.com>
2022-02-16 18:05:10 -0300

    Add responsividade e grafico, falta ajustar grafico

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let rendimentoEscolhido;
 let indexacaoEscolhido;
 $(function() {
     getIndicadores()
+   
 })
 
 function validateForm() {
@@ -42,11 +43,7 @@ async function getIndicadores () {
 
 
 async function simular() {
-    let validacao = validateForm();
     tiposSimulacao();
-    let rendimento = document.getElementsByName('rendimento');
-    let tipoIndexacao = document.getElementsByName('indexacao');
-    console.log(rendimento, tipoIndexacao)
     if(validateForm()) {
         const response = await fetch('http://localhost:3000/simulacoes');
         const simulacao = await response.json()
@@ -67,6 +64,7 @@ function tiposSimulacao() {
         console.log(tipoRendimento[i].checked)
         if(tipoRendimento[i].checked) {
             rendimentoEscolhido = tipoRendimento[i].id
+
         }
     }
     for(let i = 0; i < tipoIndexacao.length; i++) {
@@ -147,7 +145,71 @@ function renderCards(obj) {
     valorTotalInvestido.appendChild(valorTotal);
     ganhoLiquido.appendChild(labelGanhoLiquido);
     ganhoLiquido.appendChild(GanhoLiquido);
+
+    rendeGrafico(obj.graficoValores)
 }
+
+function rendeGrafico (array) {
+    console.log(array.comAporte[1]);
+    var arrayComAporte = [];
+    var arraySemAporte = [];
+    for(let i=0; i<10; i++) {
+        arrayComAporte.push([array.comAporte[i]])
+        arraySemAporte.push([array.semAporte[i]])
+        console.log(array.comAporte[i])
+
+    }
+    console.log(arrayComAporte,arraySemAporte);
+
+
+    // Load the Visualization API and the corechart package.
+    google.charts.load('current', {'packages':['corechart']});
+
+    // Set a callback to run when the Google Visualization API is loaded.
+    google.charts.setOnLoadCallback(drawChart);
+
+    // Callback that creates and populates a data table,
+    // instantiates the pie chart, passes in the data and
+    // draws it.
+    function drawChart() {
+        // Create the data table.
+        var data = google.visualization.arrayToDataTable([
+            ['Tempo (meses)','Sem Aporte', 'Com Aporte'],
+            ['0', array.semAporte[0], array.comAporte[0]],
+            ['1', array.semAporte[1], array.comAporte[1]],
+            ['2', array.semAporte[2], array.comAporte[2]],
+            ['3', array.semAporte[3], array.comAporte[3]],
+            ['4', array.semAporte[4], array.comAporte[4]],
+            ['5', array.semAporte[5], array.comAporte[5]],
+            ['6', array.semAporte[6], array.comAporte[6]],
+            ['7', array.semAporte[7], array.comAporte[7]],
+            ['8', array.semAporte[8], array.comAporte[8]],
+            ['9', array.semAporte[9], array.comAporte[9]],
+            ['10', array.semAporte[10], array.comAporte[10]]
+          ]);
+
+        // Set chart options
+        var options = {
+            width: 800,
+            height: 200,
+            title: "Projeção de Valores",
+            legend: { position: 'bottom', maxLines: 4 },
+            bar: { groupWidth: '75%' },
+            isStacked: true,
+            vAxis: {
+                title: 'Valor (R$)'
+              },
+            hAxis: {
+                title: "Tempo (meses)"
+            }
+              
+          };
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.ColumnChart(document.getElementById('grafico'));
+        chart.draw(data, options);
+    } 
+}
+
 
 function limparCampos(parent) {
     while(parent.firstChild){
